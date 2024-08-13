5e35bb67e5c0aeaebb9f8ef3650808101d54417a
GreeNox <saydulla2410@gmail.com>
2024-08-13 17:03:38 +0500

    Create new branch. And create user model in pdp_academy.dart file

--- lib/pdp_academy.dart
+++ lib/pdp_academy.dart
@@ -1,1 +1,16 @@
+void main() {
+  print("Create new branch. This name is greenox");
+}
 
+class User {
+
+  final String name;
+  final int age;
+
+  User({required this.name, required this.age});
+
+  @override
+  String toString() {
+    return "Name: $name, Age: $age";
+  }
+}
